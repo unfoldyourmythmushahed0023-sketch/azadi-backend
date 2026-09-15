c2dee4159b2275994716249b36eb95faba8b4f6a
Sofia Mushahed <afghan.girl.education0022@gmail.com>
2026-09-14 19:58:42 -0700

    Fix: Use MONGODB_URI environment variable for production

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 const { MongoClient } = require('mongodb');
 
-const uri = 'mongodb://localhost:27017';
+const uri = process.env.MONGODB_URI || 'mongodb://localhost:27017';
 const dbName = 'azadi_platform';
 
 let db;
